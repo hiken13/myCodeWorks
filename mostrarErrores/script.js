f4376a38ea7be4489176af63a62cfcb6dc1925cc
hiken13 <mancraft13@hotmail.es>
2015-09-28 01:03:54 -0600

    Editar completo con fotos 1:23 27 septiembre

---
+++ mostrarErrores/script.js
@@ -0,0 +1,5 @@
+
+function Desa(){ 
+    document.getElementById("modal1").innerHTML=""; 
+}; 
+window.load=setTimeout('Desa()', 5000); 
---
+++ mostrarErrores/ventanaModal.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style.css">
+<div id="modal1" class="modalmask">
+    <div class="modalbox movedown">
+        <a href="#close" title="Close" class="close">X</a>
+        <h2>DESLIZAR</h2>
+        <p>La ventana modal aparece por arriba y se desliza hasta su posición. Un efecto simple pero elegante.</p>
+        <p>Aquí puedes incluir cualquier cosa como vídeos, mapas, formularios...</p>
+    </div>
+</div>
